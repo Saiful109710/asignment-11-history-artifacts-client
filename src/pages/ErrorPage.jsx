ccc0f3f4c2e25973e38f280b194693d51fd52083
saiful-islam <saifulislam109710@gmail.com>
2025-01-08 23:17:07 +0600

    add 404 page and modified image

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import { useNavigate } from "react-router-dom";
+import errorPhoto from '../assets/images/errorPhoto.jpg'
 // Lottie Player
 
 const ErrorPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-sky-100">
+    <div className="flex  justify-center items-center gap-10 min-h-screen bg-sky-100">
       <Helmet>
               <title>Error | History Artifact</title>
             </Helmet>
       {/* Lottie Animation */}
-     
+     <div>
+        <img className="max-w-[400px]" src={errorPhoto} alt="" />
+     </div>
 
       {/* 404 Message */}
-      <h1 className="text-5xl font-bold text-sky-700 mt-4">404</h1>
+     <div>
+     <h1 className="text-5xl font-bold text-sky-700 mt-4">404</h1>
       <p className="text-xl text-gray-600 mt-2">
         Oops! The page you're looking for doesn't exist.
       </p>
@@ -27,6 +31,7 @@ const ErrorPage = () => {
       >
         Go Back Home
       </button>
+     </div>
     </div>
   );
 };
